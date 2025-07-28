183459ebbdbe0813ba2d7937710138f7a7be1dad
MaxiCarrillo <maxi11carrillo@gmail.com>
2025-07-28 00:02:45 -0300

    fix: Update link text from "Cargar dinero" to "Ingresar dinero" in MoneyAndPay component

--- src/app/(main)/dashboard/components/money-and-pay.tsx
+++ src/app/(main)/dashboard/components/money-and-pay.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const MoneyAndPay = () => {
     return (
         <section className='flex flex-col sm:flex-row gap-4'>
-            <Link href="/dashboard/deposit" className="w-full py-6 bg-accent rounded-md text-background text-center font-bold shadow-md">Cargar dinero</Link>
+            <Link href="/dashboard/deposit" className="w-full py-6 bg-accent rounded-md text-background text-center font-bold shadow-md">Ingresar dinero</Link>
             <Link href="/dashboard/payments" className="w-full py-6 bg-accent rounded-md text-background text-center font-bold shadow-md">Pago de servicios</Link>
         </section>
     )
